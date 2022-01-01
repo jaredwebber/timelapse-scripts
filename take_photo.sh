b8fb6e247690183e9365d7ed114df748c98f46d6
jaredwebber <jaredwebberdev@gmail.com>
2022-01-01 14:28:02 -0600

    Update take_photo.sh

--- take_photo.sh
+++ take_photo.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-HOME=/root
-LOGNAME=root
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-LANG=en_US.UTF-8
-SHELL=/bin/sh
-PWD=/root
-
-cd /media/pi/SSD/Images
-gphoto2 --camera "Canon EOS 1200D" --capture-image-and-download --filename "%Y.%m.%d.%H.%M.%S.CR2"
+cd /media/pi/SSD
+gphoto2 --capture-image-and-download --filename "%Y.%m.%d.%H.%M.%S.CR2"
